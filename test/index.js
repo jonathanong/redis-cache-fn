48b626f890aa54a9cbe1a2190c32a17b1dd1134d
jongleberry <me@jongleberry.com>
2016-11-01 16:55:29 +0000

    tests++

---
+++ test/index.js
@@ -0,0 +1,257 @@
+'use strict'
+
+/* eslint-env mocha */
+/* eslint max-nested-callbacks: 0 */
+
+const assert = require('assert')
+const Redis = require('ioredis')
+
+const client = Redis.createClient()
+const subscriber = Redis.createClient()
+
+const Cache = require('..')({
+  client,
+  subscriber
+})
+
+before(() => {
+  return client.flushall()
+})
+
+describe('Concurrency', () => {
+  describe('encoding=`json`', () => {
+    it('should not allow concurrent execution of an asynchronous function', () => {
+      let called = 0
+      const fn = Cache.extend({
+        namespace: createNamespace()
+      }).wrap(val => {
+        return wait(10).then(() => {
+          called++
+          return val + 1
+        })
+      })
+
+      return Promise.all([
+        fn(1),
+        fn(1)
+      ]).then(results => {
+        // equal results
+        assert.deepEqual(results, [2, 2])
+        // only called once
+        assert.equal(called, 1)
+      })
+    })
+
+    it('should not allow concurrent execution of a synchronous function', () => {
+      let called = 0
+      const rand = Math.random()
+      const fn = Cache.extend({
+        namespace: createNamespace()
+      }).wrap(val => {
+        called++
+        return val + rand
+      })
+
+      return Promise.all([
+        fn(1),
+        fn(1)
+      ]).then(results => {
+        // equal results
+        assert.deepEqual(results, [1 + rand, 1 + rand])
+        // only called once
+        assert.equal(called, 1)
+      })
+    })
+  })
+
+  describe('encoding=`buffer`', () => {
+    it('should not allow concurreny execution of an asynchronous function', () => {
+      let called = 0
+      const fn = Cache.extend({
+        namespace: createNamespace(),
+        encoding: 'buffer',
+        pollInterval: 10000
+      }).wrap(val => {
+        called++
+        return wait(100).then(() => {
+          return new Buffer(val, 'hex')
+        })
+      })
+
+      const hex = '00ff44'
+
+      return Promise.all([
+        fn(hex),
+        fn(hex)
+      ]).then(results => {
+        for (const result of results) {
+          assert.equal(result.toString('hex'), hex)
+        }
+        assert.equal(called, 1)
+      })
+    })
+  })
+})
+
+describe('Caching', () => {
+  describe('encoding=`json`', () => {
+    it('should cache results', () => {
+      let called = 0
+
+      const fn = Cache.extend({
+        namespace: createNamespace()
+      }).wrap(val => {
+        return wait(10).then(() => {
+          called++
+          return val + 1
+        })
+      })
+
+      const promise = fn(1)
+      assert.equal('string', typeof promise.cache.HASH)
+
+      return promise.then(val => {
+        assert.equal(val, 2)
+        return fn(1).then(val => {
+          assert.equal(val, 2)
+          assert.equal(called, 1)
+        })
+      })
+    })
+  })
+
+  describe('encoding=`buffer`', () => {
+    it('should cache results', () => {
+      let called = 0
+      const fn = Cache.extend({
+        namespace: createNamespace(),
+        encoding: 'buffer',
+        fn(val) {
+          called++
+          return new Buffer(val, 'hex')
+        }
+      }).createWrappedFunction()
+
+      const hex = '00ff44'
+
+      return fn(hex).then(val => {
+        assert(Buffer.isBuffer(val))
+        assert.equal(val.toString('hex'), hex)
+        return fn(hex)
+      }).then(val => {
+        assert(Buffer.isBuffer(val))
+        assert.equal(val.toString('hex'), hex)
+        assert.equal(called, 1)
+      })
+    })
+  })
+
+  describe('encoding=`string`', () => {
+    it('should cache results', () => {
+      let called = 0
+      const fn = Cache.extend({
+        namespace: createNamespace(),
+        encoding: 'string'
+      }).wrap(val => {
+        called++
+        return String(val) + String(val)
+      })
+
+      const string = 'asdf'
+
+      return fn(string).then(val => {
+        assert.equal(val, string + string)
+        return fn(string)
+      }).then(val => {
+        assert.equal(val, string + string)
+        assert.equal(called, 1)
+      })
+    })
+  })
+
+  describe('ttl=0', () => {
+    it('should not cache w/ ttl=0', () => {
+      let called = 0
+      const fn = Cache.extend({
+        namespace: createNamespace(),
+        ttl: 0
+      }).wrap(val => {
+        called++
+        return val
+      })
+
+      return fn(1).then(val => {
+        assert.equal(val, 1)
+        return fn(1)
+      }).then(val => {
+        assert.equal(val, 1)
+        assert.equal(called, 2)
+      })
+    })
+  })
+})
+
+describe('Error Handling', () => {
+    it('should return the same error w/ asynchronous functions', () => {
+      let called = 0
+
+      const fn = Cache.extend({
+        namespace: createNamespace()
+      }).wrap(val => {
+        return wait(100).then(() => {
+          called++
+          throw new Error('boom')
+        })
+      })
+
+      return Promise.all([
+        fn(1).then(() => {
+          throw new Error('nope')
+        }).catch(err => {
+          assert.equal(err.message, 'boom')
+        }),
+        fn(1).then(() => {
+          throw new Error('nope')
+        }).catch(err => {
+          assert.equal(err.message, 'boom')
+        })
+      ]).then(() => {
+        assert.equal(called, 1)
+      })
+    })
+
+    it('should return the same error w/ synchronous functions', () => {
+      // difference is that we don't care if the function is called multiple times
+      // when the function does not take a lot of time
+      const fn = Cache.extend({
+        namespace: createNamespace()
+      }).wrap(val => {
+        throw new Error('boom')
+      })
+
+      return Promise.all([
+        fn(1).then(() => {
+          throw new Error('nope')
+        }).catch(err => {
+          assert.equal(err.message, 'boom')
+        }),
+        fn(1).then(() => {
+          throw new Error('nope')
+        }).catch(err => {
+          assert.equal(err.message, 'boom')
+        })
+      ])
+    })
+  })
+
+function wait (ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
+
+function noop () {}
+
+function createNamespace () {
+  return Math.random().toString()
+}
